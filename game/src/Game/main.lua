b30efa8b4fbeba3df50870dc1291aa5706043199
れみろう <remyroez+github@gmail.com>
2019-06-10 22:39:29 +0900

    :sparkles: フィールドのズームと移動を実装

--- game/src/Game/main.lua
+++ game/src/Game/main.lua
@@ -15,29 +15,68 @@ end
 
 -- 読み込み
 function Game:load(...)
+    -- スクリーンサイズ
+    self.width, self.height = love.graphics.getDimensions()
+
+    -- フィールド
     self.field = Field{  }
+
+    -- 移動モード
+    self.move = false
+    self.moveOrigin = { x = 0, y = 0 }
+    self.offsetOrigin = { x = 0, y = 0 }
+    self.offset = { x = 0, y = 0 }
+    self.zoom = 0
 end
 
 -- 更新
 function Game:update(dt, ...)
+    -- 中クリック
+    if love.mouse.isDown(3) then
+        if not self.move then
+            -- 移動モード開始
+            self.move = true
+            self.moveOrigin.x, self.moveOrigin.y = love.mouse.getPosition()
+            self.offsetOrigin.x, self.offsetOrigin.y = self.offset.x, self.offset.y
+        else
+            -- 移動中
+            local x, y = love.mouse.getPosition()
+            self:setOffset(self.offsetOrigin.x + x - self.moveOrigin.x, self.offsetOrigin.y + y - self.moveOrigin.y)
+        end
+    else
+        if self.move then
+            -- 移動モード終了
+            self.move = false
+        end
+    end
+
     self.field:update(dt)
 end
 
 -- 描画
 function Game:draw(...)
-    self.field:draw()
+    -- フィールド描画
+    love.graphics.push()
+    do
+        love.graphics.translate(self.offset.x, self.offset.y)
+        love.graphics.scale(self:scale())
+        self.field:draw()
+    end
+    love.graphics.pop()
 
+    -- デバッグ
     love.graphics.setColor(1, 1, 1)
-    local str = ''
+    local str = 'scale = ' .. self:scale() .. ', zoom = ' .. self.zoom .. '\n'
     local x, y = love.mouse.getPosition()
-    str = str .. 'x = ' .. x .. ', y = ' .. y
+    x, y = (x - self.offset.x) / self:scale(), (y - self.offset.y) / self:scale()
+    str = str .. 'x = ' .. x .. ', y = ' .. y .. '\n'
     local square = self.field:getSquare(x, y)
     if square then
-        str = str .. ', animal = ' .. square.nutrients.animal .. ''
+        str = str .. 'animal = ' .. square.nutrients.animal .. ''
         str = str .. ', plantal = ' .. square.nutrients.plantal .. ''
         str = str .. ', mineral = ' .. square.nutrients.mineral .. ''
     end
-    love.graphics.print(str, 0, 50)
+    love.graphics.print(str)
 end
 
 -- キー入力
@@ -66,8 +105,36 @@ end
 
 -- マウスホイール
 function Game:wheelmoved(x, y)
+    if y < 0 and self.zoom > -9 then
+        -- ズームアウト
+        self.zoom = self.zoom - 1
+        self:setOffset()
+    elseif y > 0 and self.zoom < 9 then
+        -- ズームイン
+        self.zoom = self.zoom + 1
+        self:setOffset()
+    end
 end
 
 -- リサイズ
 function Game:resize(width, height)
+    self.width, self.height = width, height
+    self:setOffset()
+end
+
+-- リサイズ
+function Game:scale()
+    if self.zoom == 0 then
+        return 1
+    elseif self.zoom > 0 then
+        return self.zoom + 1
+    elseif self.zoom < 0 then
+        return 1 / math.abs(self.zoom - 1)
+    end
+end
+
+-- オフセットの設定
+function Game:setOffset(x, y)
+    self.offset.x = math.max(-self.field.width * self:scale() + self.width * 0.5, math.min(x or self.offset.x, self.width * 0.5))
+    self.offset.y = math.max(-self.field.height * self:scale() + self.height * 0.5, math.min(y or self.offset.y, self.height * 0.5))
 end
